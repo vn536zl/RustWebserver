09a1a7686976cf8e4235869b43cca1d105a865da
John <jhaywa02@gmail.com>
2023-01-02 14:06:55 -0600

    simple http server start

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use std::{
+    fs,
+    time::Duration,
+};
+use async_std::{
+    task,
+    prelude::*,
+    net::{TcpListener, TcpStream}
+};
+
+#[async_std::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream).await;
+    }
+}
+
+
+async fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1";
+    let sleep = b"GET /sleep HTTP/1.1";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "src/html/content/index.html")
+    } else if buffer.starts_with(sleep) {
+        task::sleep(Duration::from_secs(5)).await;
+        ("HTTP/1.1 200 OK", "src/html/content/index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "src/html/errors/404.html")
+    };
+
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).await.unwrap();
+    stream.flush().await.unwrap();
+}
